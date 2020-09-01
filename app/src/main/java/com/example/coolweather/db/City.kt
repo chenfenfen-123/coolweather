9afd6e6d06a9addfd15baacb7c1ee13dd009c14e
chenfenfen <cff155@163.com>
2020-09-01 15:21:34 +0800

    加入创建数据库各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.kt
@@ -0,0 +1,5 @@
+package com.example.coolweather.db
+
+import org.litepal.crud.LitePalSupport
+
+class City(val id: Int, val cityName: String, val cityCode: Int, val provinceId: Int): LitePalSupport()
---
+++ app/src/main/java/com/example/coolweather/db/County.kt
@@ -0,0 +1,5 @@
+package com.example.coolweather.db
+
+import org.litepal.crud.LitePalSupport
+
+class County(val id: Int, val countyName: String, val weatherId: String, val cityId: Int): LitePalSupport()
